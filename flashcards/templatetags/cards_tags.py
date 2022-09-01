81cda49a7f2bff846b1ff375c4c7091465fb45e8
coreygross <grosscor3@gmail.com>
2022-09-01 11:39:49 -0400

    finished with boxes and css completed

--- flashcards/templatetags/cards_tags.py
+++ flashcards/templatetags/cards_tags.py
@@ -9,10 +9,8 @@ register = template.Library()
 def boxes_as_links():
     boxes = []
     for box_num in BOXES:
-        card_count = Card.objects.filter(box=box_num).count()
         boxes.append({
             "number": box_num,
-            "card_count": card_count,
         })
 
     return {"boxes": boxes}
